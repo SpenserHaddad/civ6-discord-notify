0dc69ab9706937b56c9076f524c7e36a6cbbc76a
Spenser Haddad <spenser.haddad@gmail.com>
2020-06-07 01:42:35 -0400

    Create Discord bot instance on app startup

--- civ6_notify/server.py
+++ civ6_notify/server.py
@@ -1,7 +1,9 @@
+import asyncio
 import json
-from os import path
+from os import path, getenv
 from quart import Quart, request
 from .game_tracker import CivGameTracker
+from .discord_bot import CivDiscordNotifyBotClient, add_commands_to_bot
 
 app = Quart(__name__)
 
@@ -11,6 +13,17 @@ with open(sample_json_path) as f:
     game_data = json.load(f)
 
 game_tracker = CivGameTracker.from_dict(game_data)
+
+
+@app.before_serving
+async def start_discord_client():
+    token = getenv("CIV6_NOTIFY_DISCORD_TOKEN")
+    loop = asyncio.get_event_loop()
+    discord_bot = CivDiscordNotifyBotClient(loop)
+    add_commands_to_bot(discord_bot)
+
+    game_tracker.discord_client = discord_bot
+    loop.create_task(discord_bot.start(token))
 
 
 @app.route("/civ6", methods=["POST"])
